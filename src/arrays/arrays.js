53d5f7be9891912f66f057db3379f02e14045ad9
PYXHD <marc-charvot@hotmail.fr>
2026-02-08 10:46:56 +0100

    implement sum(array) and improve test coverage

--- src/arrays/arrays.js
+++ src/arrays/arrays.js
@@ -35,8 +35,27 @@ function chunk(arr, size) {
     return result;
 }
 
+function sum(arr) {
+    if (!Array.isArray(arr)) {
+        throw new TypeError("First argument must be an array");
+    }
+    if (arr.length === 0) {
+        return 0;
+    }
+
+    let total = 0;
+    for (const n of arr) {
+        if (!Number.isFinite(n)) {
+            throw new TypeError("Array must contain only numbers");
+        }
+        total += n;
+    }
+    return total;
+}
+
 export {
     getMax,
     unique,
     chunk,
+    sum
 };
--- tests/arrays/arrays.test.js
+++ tests/arrays/arrays.test.js
@@ -1,4 +1,4 @@
-import { getMax, unique, chunk } from "../../src/arrays"
+import { getMax, unique, chunk, sum } from "../../src/arrays"
 
 describe("Array utilities", () => {
 
@@ -146,4 +146,51 @@ describe("Array utilities", () => {
 
     });
 
+    // ======================
+    // sum()
+    // ======================
+    describe("sum()", () => {
+
+        describe("normal cases", () => {
+            test.each([
+                [[1, 2, 3], 6],
+                [[0], 0],
+                [[-1, 1], 0],
+                [[2.5, 1.5], 4]
+            ])("sum(%p) returns %d", (input, expected) => {
+                expect(sum(input)).toBe(expected);
+            })
+        });
+
+
+        describe("edge cases", () => {
+            test("returns the sum even only one value", () => {
+                const array = [2];
+                expect(sum(array)).toBe(2);
+            });
+            test("returns 0 if empty array", () => {
+                const array = [];
+                expect(sum(array)).toBe(0);
+            });
+        });
+
+        describe("error cases", () => {
+            test("throws is not an array", () => {
+                expect(() => {
+                    sum("apple");
+                }).toThrow(TypeError);
+            });
+
+            test.each([
+                [["banana", 1]],
+                [[1, undefined]],
+                [[1, null]],
+                [[undefined]],
+                [[null]],
+            ])("throws if array contains invalid values: %p", (input) => {
+                expect(() => sum(input)).toThrow(TypeError);
+            });
+        });
+
+    });
 });
